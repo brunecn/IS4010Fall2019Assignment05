106cc543b712ff2b82961a3ec44823fb5a6ebf72
Christian <Christian@DESKTOP-V2V2NM0>
2019-10-02 16:28:55 -0400

    Assignment 05

--- IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -8,24 +8,57 @@ package footballFun;
 
 public class FootballFun {
 
-	public static void main(String[] args) {
-		int correctCount = 0;
-		if (translateScore(2).equals("safety") == true) { correctCount++;}
-		if (translateScore(3).equals("field goal") == true) { correctCount++;}
-		if (translateScore(6).equals("touchdown") == true) { correctCount++;}
-		if (translateScore(7).equals("touchdown and extra point") == true) { correctCount++;}
-		if (translateScore(8).equals("touchdown and 2-point conversion") == true) { correctCount++;}
-		if (translateScore(-1).equals("") == true) { correctCount++;}
-		if (translateScore(1).equals("invalid") == true) { correctCount++;}
-		if (translateScore(10).equals("you must be playing Quidditch ") == true) { correctCount++;}
-		
-		if (correctCount == 8) {
-			System.out.println("All tests passed");
-		} else {
-			System.out.println("At least one test failed");
-		}
-	}
-	public static String translateScore (int score) {
-		return "";
-	}
-}
+    public static void main(String[] args) {
+        int correctCount = 0;
+        if (translateScore(2).equals("safety") == true) { correctCount++;}
+        if (translateScore(3).equals("field goal") == true) { correctCount++;}
+        if (translateScore(6).equals("touchdown") == true) { correctCount++;}
+        if (translateScore(7).equals("touchdown and extra point") == true) { correctCount++;}
+        if (translateScore(8).equals("touchdown and 2-point conversion") == true) { correctCount++;}
+        if (translateScore(-1).equals("") == true) { correctCount++;}
+        if (translateScore(1).equals("invalid") == true) { correctCount++;}
+        if (translateScore(10).equals("you must be playing Quidditch ") == true) { correctCount++;}
+
+        if (correctCount == 8) {
+            System.out.println("All tests passed");
+        } else {
+            System.out.println("At least one test failed");
+        }
+    }
+    public static String translateScore (int score) {
+        String scoreDisc = "";
+
+        switch(score) {
+        case 2:
+            scoreDisc = "safety";
+            break;
+        case 3:
+            scoreDisc = "field goal";
+            break;
+        case 6:
+            scoreDisc = "touchdown";
+            break;
+        case 7:
+            scoreDisc = "touchdown and extra point";
+            break;
+        case 8:
+            scoreDisc = "touchdown and 2-point conversion";
+            break;
+        case -1:
+            scoreDisc = "";
+            break;
+        case 1:
+            scoreDisc = "invalid";
+            break;
+        case 10:
+    		scoreDisc = "you must be playing Quidditch ";
+    		break;
+            
+        }
+        return scoreDisc;
+        
+    }
+
+      
+    }
+
